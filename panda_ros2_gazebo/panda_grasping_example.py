77d71e8a22dbebc03f58f53aabeb65d920121de5
Nico Palomo <npalomo@student.ethz.ch>
2021-06-13 01:01:16 -0700

    wip

--- panda_ros2_gazebo/panda_grasping_example.py
+++ panda_ros2_gazebo/panda_grasping_example.py
@@ -6,6 +6,7 @@
 
 import enum
 import copy
+import numpy as np
 
 # ROS2 Python API libraries
 import rclpy
@@ -22,6 +23,9 @@ from .scripts.models.panda import Panda, FingersAction
 
 # Helper class for RViz visualization
 from .rviz_helper import RVizHelper
+
+# Misc
+from .panda_pick_and_place import quat_mult
 
 class StateMachineAction(enum.Enum):
 
@@ -97,7 +101,7 @@ class PandaPickAndPlace(Node):
         if self.end_effector_reached():
             # sample a new end effector target
             # self.get_logger().info('END EFFECTOR TARGET REACHED!')
-            self.go_to_next_target() # cycle to next action in state machine
+            self.get_next_target() # cycle target to next action in state machine
 
         # Publish the end effector target and odometry messages
         self._end_effector_target_publisher.publish(self._end_effector_target)
@@ -108,4 +112,68 @@ class PandaPickAndPlace(Node):
 
         self.joint_group_position_controller_callback()
 
+    def end_effector_reached(self,
+                            max_error_pos: float = 0.05,
+                            max_error_rot: float = 0.05,
+                            max_error_vel: float = 0.1,
+                            mask: np.ndarray = np.array([1., 1., 1.])) -> bool:
+
+        # Check the position to see if the target has been reached
+        position = np.array([
+            self._end_effector_current.pose.pose.position.x,
+            self._end_effector_current.pose.pose.position.y,
+            self._end_effector_current.pose.pose.position.z])
+        velocity = np.array([
+            self._end_effector_current.twist.twist.linear.x,
+            self._end_effector_current.twist.twist.linear.y,
+            self._end_effector_current.twist.twist.linear.z,
+            self._end_effector_current.twist.twist.angular.x,
+            self._end_effector_current.twist.twist.angular.y,
+            self._end_effector_current.twist.twist.angular.z])
+        target = np.array([
+            self._end_effector_target.pose.pose.position.x,
+            self._end_effector_target.pose.pose.position.y,
+            self._end_effector_target.pose.pose.position.z])
+
+        masked_target = mask * target
+        masked_current = mask * position
+
+        end_effector_reached = (np.linalg.norm(masked_current - masked_target) < max_error_pos) and \
+            (np.linalg.norm(velocity[:3]) < max_error_vel)
+
+        # Check the orientation to see if the target has been reached
+        orientation = np.array([
+            self._end_effector_current.pose.pose.orientation.x,
+            self._end_effector_current.pose.pose.orientation.y,
+            self._end_effector_current.pose.pose.orientation.z,
+            self._end_effector_current.pose.pose.orientation.w])
+        target_inv = np.array([
+            -self._end_effector_target.pose.pose.orientation.x,
+            -self._end_effector_target.pose.pose.orientation.y,
+            -self._end_effector_target.pose.pose.orientation.z,
+            self._end_effector_target.pose.pose.orientation.w])
+        target_inv /= np.linalg.norm(target_inv)
+
+        # find the rotation difference between the two quaternions
+        orientation_diff = quat_mult(orientation, target_inv)
+        rot_vec = R.from_quat(orientation_diff).as_rotvec()
+
+        end_effector_reached = end_effector_reached and (np.pi - np.linalg.norm(rot_vec) < max_error_rot) and (np.linalg.norm(velocity[3:]) < max_error_vel)
+
+        # self.get_logger().info('ERROR NORM:\n{}'.format(np.linalg.norm(masked_current - masked_target)))
+        # self.get_logger().info('END EFFECTOR POSE:\n{}'.format(self._end_effector_current.pose.pose.position))
+        # self.get_logger().info('END EFFECTOR TARGET:\n{}'.format(self._end_effector_target.pose.pose.position))
+
+        return end_effector_reached
+
+    def joint_group_position_controller_callback(self) -> None:
+
+        msg = Float64MultiArray()
+        msg.data = list(self._joint_targets)
+        self._joint_commands_publisher.publish(msg)
+
         # TODO: Download the wood_cube_5cm model; Specify the state machine
+
+    def get_next_target(self):
+
+        print("Do stuff. Get the next target task for the Panda.")
