773c8217691f846fb92f331c5d6d82cea2be0112
Nico Palomo <njpalomo@outlook.com>
2021-10-23 18:12:10 -0700

    updated docker file

--- docker/entrypoint.bash
+++ docker/entrypoint.bash
@@ -5,15 +5,18 @@ cd ${WORKSPACE_DIR}
 
 source /opt/ros/${ROS2_DISTRO}/setup.bash
 
-MAKEFLAGS="-j1" colcon build --merge-install --executor sequential --cmake-args -DIDYNTREE_USES_PYTHON=True -DIDYNTREE_USES_IPOPT:BOOL=ON -DCMAKE_BUILD_TYPE=Release
+if [ "$1" = "build" ]; then
 
-source install/setup.bash
+    MAKEFLAGS="-j1" colcon build --merge-install --parallel-workers 1 --cmake-args -DIDYNTREE_USES_PYTHON=True -DIDYNTREE_USES_IPOPT:BOOL=ON -DCMAKE_BUILD_TYPE=Release
 
-if [ "$1" = "teleop" ]; then
-    ros2 launch panda_ros2_gazebo gazebo.launch.py &
-    ros2 launch panda_ros2_gazebo bringup.launch.py mode:=$1 &
-    ros2 launch panda_teleop panda_teleop_control.launch.py
 else
-    ros2 launch panda_ros2_gazebo gazebo.launch.py &
-    ros2 launch panda_ros2_gazebo bringup.launch.py mode:=$1
+
+    source install/setup.bash
+
+    if [ "$1" = "teleop" ]; then
+        ros2 launch panda_ros2_gazebo gazebo.launch.py & ros2 launch panda_ros2_gazebo bringup.launch.py mode:=$1 & ros2 launch panda_teleop panda_teleop_control.launch.py & wait
+    else
+        ros2 launch panda_ros2_gazebo gazebo.launch.py & ros2 launch panda_ros2_gazebo bringup.launch.py mode:=$1 & wait
+    fi
+
 fi
